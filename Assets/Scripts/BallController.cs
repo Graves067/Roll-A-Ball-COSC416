a1afc484f493a8d784a9ba06512b2533ca574372
Graves067 <c.a.gillespie9@gmail.com>
2025-01-26 23:50:15 -0800

    Ball jumps

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,6 +8,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private Boolean Grounded = true;
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float jumpForce = 2.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -38,10 +39,23 @@ public class BallController : MonoBehaviour
          if (Input.GetKey(KeyCode.A))
           {
           inputVector += Vector3.left;
-          }
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Grounded = false;
+        }
 
         Vector3 inputXZPlane = new(inputVector.x, 0, inputVector.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Grounded = true;
+        }
     }
 }
